6ff3c7884c67437af5c2daeb5834e9f16ec38b08
benjamin77-git <m.mugishabenjamin@gmail.com>
2024-12-02 11:58:57 +0200

    [npm framework]

---
+++ index.js
@@ -0,0 +1,13 @@
+import inquirer from "inquirer";
+inquirer
+  .prompt([{ message: "Type your message here", name: "URL" }])
+  .then((answers) => {
+    console.log(answers);
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
